d50c831a9aa43ef58bff7fc99bfa610b5d0faa73
mmaquina <mmaquina@gmail.com>
2022-11-06 00:00:57 -0300

    reverted fix

--- app/controllers/order.py
+++ app/controllers/order.py
@@ -12,7 +12,7 @@ class OrderController(BaseController):
 
     @staticmethod
     def calculate_order_price(size_price: float, ingredients: list):
-        price = sum(ingredient.price for ingredient in ingredients) + size_price
+        price = sum(ingredient.price for ingredient in ingredients)
         return round(price, 2)
 
     @classmethod
