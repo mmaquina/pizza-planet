75d121cf32625bd7852b9ca64671c825fb8e7317
mmaquina <mmaquina@gmail.com>
2022-11-07 14:51:40 -0300

    fixed test_order, now testing all 3 services

--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -3,26 +3,20 @@ import pytest
 from app.test.utils.functions import get_random_string, get_random_price
 
 
-def test_create_orders_service(create_orders):
-    order = create_orders[0].json
-    pytest.assume(create_orders[0].status.startswith('200'))
-    pytest.assume(order['_id'])
-    pytest.assume(order['name'])
-    pytest.assume(order['price'])
+def test_create_order_service(create_order):
+    order = create_order.json
+    pytest.assume(create_order.status.startswith('200'))
+    pytest.assume(order['client_address'])
+    pytest.assume(order['client_dni'])
+    pytest.assume(order['client_name'])
+    pytest.assume(order['client_phone'])
+    pytest.assume(order['size'])
+    pytest.assume(order['date'])
+    pytest.assume(order['total_price'])
 
 
-def test_update_order_service(client, create_orders, order_uri):
-    current_order = create_orders[0].json
-    update_data = {**current_order, 'name': get_random_string(), 'price': get_random_price(1, 5)}
-    response = client.put(order_uri, json=update_data)
-    pytest.assume(response.status.startswith('200'))
-    updated_order = response.json
-    for param, value in update_data.items():
-        pytest.assume(updated_order[param] == value)
-
-
-def test_get_order_by_id_service(client, create_orders, order_uri):
-    current_order = create_orders[0].json
+def test_get_order_by_id_service(client, create_order, order_uri):
+    current_order = create_order.json
     response = client.get(f'{order_uri}id/{current_order["_id"]}')
     pytest.assume(response.status.startswith('200'))
     returned_order = response.json
@@ -35,4 +29,4 @@ def test_get_orders_service(client, create_orders, order_uri):
     pytest.assume(response.status.startswith('200'))
     returned_orders = {order['_id']: order for order in response.json}
     for order in create_orders:
-        pytest.assume(order['_id'] in returned_orders)
+        pytest.assume(order.json['_id'] in returned_orders)
