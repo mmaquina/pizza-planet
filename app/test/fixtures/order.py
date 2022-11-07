6f25cf99ddd3cd8e8ef896dfb5f5f0381bbfcf90
mmaquina <mmaquina@gmail.com>
2022-11-07 19:47:33 -0300

    PIZ4 increase test coverage (#4)
    * added size service tests
    
    * added order service tests
    
    * fix order_uri in fixtures
    
    * added create_order fixture
    
    * updated ci pipeline and requirements.txt

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -15,7 +15,7 @@ def client_data_mock() -> dict:
 
 @pytest.fixture
 def order_uri():
-    return '/order'
+    return '/order/'
 
 
 @pytest.fixture
@@ -32,6 +32,18 @@ def order(create_ingredients, create_size, client_data) -> dict:
         'ingredients': ingredients,
         'size_id': size_id
     }
+
+
+@pytest.fixture
+def create_order(client, order_uri, create_ingredients, create_sizes) -> dict:
+    ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
+    sizes = [size.get('_id') for size in create_sizes]
+    new_order = client.post(order_uri, json={
+        **client_data_mock(),
+        'ingredients': shuffle_list(ingredients)[:5],
+        'size_id': shuffle_list(sizes)[0]
+    })
+    return new_order
 
 
 @pytest.fixture
