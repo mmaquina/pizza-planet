b291cfe30a30af908c6aa5afacb872b25f0b0c84
mmaquina <mmaquina@gmail.com>
2022-11-11 14:37:06 -0300

    refactored ingredient

--- app/services/size.py
+++ app/services/size.py
@@ -7,23 +7,26 @@ from .base import BaseService
 
 size = Blueprint('size', __name__)
 
+
 class SizeService(BaseService):
     controller = SizeController
+
 
 @size.route('/', methods=POST)
 def create_size():
     return SizeService.create()
     
+
 @size.route('/', methods=GET)
 def get_sizes():
     return SizeService.get_all()
+
 
 @size.route('/', methods=PUT)
 def update_size():
     return SizeService.update()
 
+
 @size.route('/id/<_id>', methods=GET)
 def get_size_by_id(_id: int):
     return SizeService.get_by_id(_id)
-
-
