8510f443ee36b780cd50f9d84358a221b689af73
mmaquina <mmaquina@gmail.com>
2022-11-11 15:55:04 -0300

    refactored base service and report service

--- app/services/base.py
+++ app/services/base.py
@@ -6,30 +6,31 @@ from ..controllers.base import BaseController
 class BaseService:
     controller: Optional[BaseController] = None
 
+    @staticmethod
+    def adapt_response(item, error):
+        response = item if not error else {'error': error}
+        status_code = 200 if item else 404 if not error else 400
+        return jsonify(response), status_code
+
     @classmethod
     def create(cls):
         item, error = cls.controller.create(request.json)
-        response = item if not error else {'error': error}
-        status_code = 200 if not error else 400
-        return jsonify(response), status_code
+        return cls.adapt_response(item, error)
 
     @classmethod
     def get_all(cls):
         item, error = cls.controller.get_all()
-        response = item if not error else {'error': error}
-        status_code = 200 if item else 404 if not error else 400
-        return jsonify(response), status_code
+        return cls.adapt_response(item, error)
+
         
     @classmethod
     def update(cls):
         item, error = cls.controller.update(request.json)
-        response = item if not error else {'error': error}
-        status_code = 200 if not error else 400
-        return jsonify(response), status_code
+        return cls.adapt_response(item, error)
+
 
     @classmethod
     def get_by_id(cls, _id):
         item, error = cls.controller.get_by_id(_id)
-        response = item if not error else {'error': error}
-        status_code = 200 if item else 404 if not error else 400
-        return jsonify(response), status_code
+        return cls.adapt_response(item, error)
+
